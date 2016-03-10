9c50c63fe44c0e8505bbe114d8e9f6e74f794a39
Kalin Ringkvist <kalin@kalinflash.com>
2016-03-10 02:26:41 -0600

    now listening for currentlyMoving to disable buttons

--- maBinDirective.js
+++ maBinDirective.js
@@ -5,9 +5,16 @@ angular.module("mancalaApp")
     //watch the value from the state object, update scope.stoneCount when this bin's count changes
     scope.$watch(function(){
       console.log("watch function!");
-      return gameState.binValues[scope.binName]
+      return gameState.binValues[scope.binName];
     }, function(newVal, oldVal){
       scope.stoneCount = newVal;
+    }, false);
+
+    //watch for currentlyMoving var and disable when stones are being moved
+    scope.$watch(function(){
+      return gameState.currentlyMoving;
+    }, function(newVal, oldVal){
+      scope.disabled = newVal;
     }, false);
 
     scope.moveStones = function(){
